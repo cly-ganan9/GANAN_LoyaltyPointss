bd9ad5b1b2e1f18f0cf1dad9d2da4345756d257a
Sunthunder0813 <santanderjoseph13@gmail.com>
2026-02-21 11:41:04 +0800

    Initial commit

---
+++ GANAN_LoyaltyPoints/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace GANAN_LoyaltyPoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Philippine Airlines!\n**Loyalty Points Section**");
+
+            string username, flightCode = "PH1234";
+            int points = 0, phcode = 20;
+
+            Console.Write("Enter username: ");
+            string usernameInp = Console.ReadLine();
+
+            Console.WriteLine(usernameInp);
+          
+
+            Console.WriteLine("Available Points: "+points+"pts");
+
+            Console.Write("Proceeed to Add Points or Redeem Points? (add/redeem): ";
+            string choice = Console.ReadLine();
+
+            if (choice == "add")
+            {
+                Console.Write("**ADD POINTS**\nEnter flight code: ");
+                string code = Console.ReadLine();
+
+                if (code == flightCode)
+                {
+                    int addCode = points + phcode;
+                    Console.Write("Available Points: " + addCode);
+                }
+                else
+                {
+                    Console.WriteLine("Not Available");
+                }
+                
+            }
+            else if (choice == "redeem")
+            {
+                Console.WriteLine("**REDEEM POINTS**");
+                Console.WriteLine("1) 10% OFF Starbucks\n2) Travel Voucher to Korea\n Choose rewards: ");
+            }
+
+        }
+    }
+}
