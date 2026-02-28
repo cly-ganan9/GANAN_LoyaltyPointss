076b16fc42f489ad376f15dcb1a3eef6955f3b0c
Clyza Ganan <lqiiyxahi@gmail.com>
2026-02-28 11:37:22 +0800

    Refactor variable initialization and input validation
    Removed unnecessary variable initialization and fixed input validation for menu selection.

--- GANAN_LoyaltyPoints/Program.cs
+++ GANAN_LoyaltyPoints/Program.cs
@@ -5,14 +5,11 @@
         static void Main(string[] args)
         {
             
-            string customerName = "", passportId = "", redeemRewards, menuInp, destination;
-            int points = 0, earnedPointsList = 0, earnedCount = 0, redeemCount = 0;
+string customerName = "", passportId = "", redeemRewards, destination;
+            int points = 0, earnedPointsList = 0, earnedCount = 0, redeemCount = 0, menuInp;
             decimal ticketPrice = 0.0m;
             
             Console.WriteLine("Welcome to Philippine Airlines!\n------Loyalty Points Section------\n");
-
-          
-
 
             Console.Write("Enter Passport ID: ");
             passportId = Console.ReadLine();
@@ -29,12 +26,11 @@
            if (points < 0)
             {
                 Console.WriteLine("ERROR: Loyalty points must not be less than 0.");
-                return;
             }
             else
             {
                  Console.WriteLine("ERROR: Loyalty points must be a number.");
-                return;
+                
             }
 
             bool menu = true;
@@ -43,8 +39,8 @@
                 Console.WriteLine("\n------MENU------");
                 Console.WriteLine("1) Earn Points\n2) Redeem Points\n3) View Account\n 4) Exit");
                 Console.Write("Choose 1-4: ");
-                menuInp = Console.ReadLine();
-                if (menuInp < 0 || menuInp >= 4)
+                menuInp = Convert.ToInt32(Console.ReadLine());
+                if (menuInp < 1 || menuInp > 4)
                 {
                     Console.WriteLine("ERROR: Choose 1 to 4 only."); 
                     return;
